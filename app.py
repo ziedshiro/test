eedd053f0865a980697377206fbae74af630ea07
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2022-02-10 06:27:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template('web.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
