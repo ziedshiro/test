6dfe5a8e14124178a69c0ed867f3bf0f62a1baa3
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2022-02-10 04:23:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def index():
     return render_template('web.html')
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
