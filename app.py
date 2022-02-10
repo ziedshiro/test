87e61e425717e52382775ea50b52489088e93ac0
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2022-02-10 09:35:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
 from flask import Flask,render_template
+import pyrebase
+
+firebaseConfig={'apiKey': "AIzaSyAkMTo42DRB-G8LSfJ3AgCXwb_iedD9Z9k",
+  'authDomain': "knotice-6acbd.firebaseapp.com",
+  'databaseURL': "https://knotice-6acbd-default-rtdb.asia-southeast1.firebasedatabase.app",
+  'projectId': "knotice-6acbd",
+  'storageBucket': "knotice-6acbd.appspot.com",
+  'messagingSenderId': "64940370131",
+  'appId': "1:64940370131:web:758cfa20615773d4890c26"}
+
+firebase=pyrebase.initialize_app(firebaseConfig)
+storage=firebase.storage()
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('web.html')
+    return render_template('web.html',test1=storage.child(cloudfilename).get_url(None))
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
